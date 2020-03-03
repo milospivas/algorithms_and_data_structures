5399f4b5511c96977433f21f8e7430463b68ea78
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-03 22:09:16 +0100

    Added the QuickSort class with starting documentation

---
+++ Python/algorithms_and_data_structures_quick_sort.py
@@ -0,0 +1,12 @@
+""" quick_sort.py
+    
+    author: Miloš Pivaš, student
+"""
+
+from random import randrange
+from random import shuffle
+
+class QuickSort:
+    'A class implementing Quicksort'
+
+    version = '0.1'
