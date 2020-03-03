174d37b8402bef458c0aa72f4c686d7991f534e0
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-03 22:10:52 +0100

    Added the partition method

--- Python/algorithms_and_data_structures_quick_sort.py
+++ Python/algorithms_and_data_structures_quick_sort.py
@@ -18,3 +18,18 @@ class QuickSort:
         a[i] = a[j]
         a[j] = aux
         pass
+
+
+    @staticmethod
+    def partition(a : list, start : int, stop : int, pivot : int) -> int:
+        'Partition the list a[start:stop] around a[pivot]'
+        QuickSort.swap(a, start, pivot)
+
+        i = start
+        for j in range(start+1, stop):
+            if a[j] < a[start]:
+                QuickSort.swap(a, j, i+1)
+                i += 1
+
+        QuickSort.swap(a, start, i)
+        return i
