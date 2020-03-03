b3909a8a6e1a5e6c5958f32a165a42da32da14d1
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-03 22:09:55 +0100

    Added the swap function

--- Python/algorithms_and_data_structures_quick_sort.py
+++ Python/algorithms_and_data_structures_quick_sort.py
@@ -10,3 +10,11 @@ class QuickSort:
     'A class implementing Quicksort'
 
     version = '0.1'
+
+    @staticmethod
+    def swap(a : list, i : int, j : int) -> None:
+        'Swaps elements from a at positions i and j'
+        aux = a[i]
+        a[i] = a[j]
+        a[j] = aux
+        pass
