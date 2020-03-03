91f8b23d3d603c2ad0f49b3a2110b6c7ba3e9b24
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-03 21:02:34 +0100

    Changed appropriate types from int to size_t.

--- c++/QuickSort/QuickSort.cpp
+++ c++/QuickSort/QuickSort.cpp
@@ -1,7 +1,7 @@
 #include "QuickSort.h"
 #include <iostream>
 
-std::vector<int> QuickSort::swap(std::vector<int> &a, int i, int j) {
+std::vector<int> QuickSort::swap(std::vector<int> &a, size_t i, size_t j) {
     if (i == j) {
         return a;
     }
@@ -11,15 +11,15 @@ std::vector<int> QuickSort::swap(std::vector<int> &a, int i, int j) {
     return a;
 }
 
-int QuickSort::get_pivot(std::mt19937 gen, int start, int end) {
+size_t QuickSort::get_pivot(std::mt19937 gen, size_t start, size_t end) {
     std::uniform_int_distribution<> dis(start, end-1);
     return dis(gen);
 }
 
-int QuickSort::partition(std::vector<int> &a, int start, int end, int p) {
+size_t QuickSort::partition(std::vector<int> &a, size_t start, size_t end, size_t p) {
     swap(a, start, p);
-    int i = start;
-    for (int j = start+1; j < end; j++) {
+    size_t i = start;
+    for (size_t j = start+1; j < end; j++) {
         if (a[j] < a[start]) {
             swap(a, j, i+1);
             i++;
@@ -29,12 +29,12 @@ int QuickSort::partition(std::vector<int> &a, int start, int end, int p) {
     return i;
 }
 
-std::vector<int> QuickSort::quicksort_rec(std::vector<int> &a, int start, int end, std::mt19937 gen) {
-    int n = end - start;
+std::vector<int> QuickSort::quicksort_rec(std::vector<int> &a, size_t start, size_t end, std::mt19937 gen) {
+    size_t n = end - start; // TODO Maybe check if n < 0
     if (n <= 1) {
         return a;
     }
-    int p = get_pivot(gen, start, end);
+    size_t p = get_pivot(gen, start, end);
     p = partition(a, start, end, p);
 
     quicksort_rec(a, start, p, gen);
@@ -43,7 +43,7 @@ std::vector<int> QuickSort::quicksort_rec(std::vector<int> &a, int start, int en
 }
 
 std::vector<int> QuickSort::sort(std::vector<int> &a) {
-    int n = a.size();
+    size_t n = a.size();
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     quicksort_rec(a, 0, n, gen);
@@ -52,23 +52,18 @@ std::vector<int> QuickSort::sort(std::vector<int> &a) {
 
 int main() {
     std::vector<int> A{3, 8, 2, 5, 1, 4, 7, 6}; //3, 1, 2, 6, 4, 7};
+    size_t n = A.size();
+
+    for (size_t i = 0; i < n; i++) {
+        std::cout << A[i] << " ";
+    }
+    std::cout << std::endl;
+
     QuickSort::sort(A);
     
-    int n = A.size();
-    int cnt = 1;
-    int sol = A[n-1];
-    for (int i = 1; i < n; i++) {
-        if (A[i] == A[i-1]) {
-            cnt++;
-        }
-        else {
-            if ((cnt % 2) == 1) {
-                sol = A[i-1];
-                break;
-            }
-            cnt = 1;
-        }
+    for (size_t i = 0; i < n; i++) {
+        std::cout << A[i] << " ";
     }
-    
-    return sol;
+    std::cout << std::endl;
+    return 0;
 }
--- c++/QuickSort/QuickSort.h
+++ c++/QuickSort/QuickSort.h
@@ -1,11 +1,12 @@
 #pragma once
+#include <iostream>
 #include <vector>
 #include <random>
 
 namespace QuickSort {
-    std::vector<int> swap(std::vector<int> &a, int i, int j);
-    int get_pivot(std::mt19937 gen, int start, int end);
-    int partition(std::vector<int> &a, int start, int end, int p);
-    std::vector<int> quicksort_rec(std::vector<int> &a, int start, int end, std::mt19937 gen);
+    std::vector<int> swap(std::vector<int> &a, size_t i, size_t j);
+    size_t get_pivot(std::mt19937 gen, size_t start, size_t end);
+    size_t partition(std::vector<int> &a, size_t start, size_t end, size_t p);
+    std::vector<int> quicksort_rec(std::vector<int> &a, size_t start, size_t end, std::mt19937 gen);
     std::vector<int> sort(std::vector<int> &a);
 };
