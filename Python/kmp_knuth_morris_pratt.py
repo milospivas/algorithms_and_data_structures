83c055b87ecb43460075a2c86d8f3ab03bc7dc7a
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-09 14:20:47 +0100

    Added detailed comments

--- Python/kmp_knuth_morris_pratt.py
+++ Python/kmp_knuth_morris_pratt.py
@@ -13,21 +13,24 @@ class KMP:
         if len(p) == 0:
             return []
 
-        t = [0 for _ in p]
+        t = [0 for _ in p]  # fill the table with 0
 
         j = 0
         for i in range(1, len(p)):
             if p[i] == p[j]:
-                t[i] = t[i-1] + 1
-                j += 1
+            # if there's a continuing match
+                t[i] = t[i-1] + 1   # increment the table entry
+                j += 1              # move j
             else:
+            # if there isn't a continuing match
                 while j > 0 and p[i] != p[j]:
-                    j = t[j-1]
+                # while we're in the pattern and there isn't a character match:
+                    j = t[j-1]  # since we've already matched with characters p[:t[j-1]] characters, try matching the next one
 
                 if p[i] == p[j]:
-                    t[i] = t[j] + 1
-                    j += 1
-            
+                # if there is an earlier match
+                    t[i] = t[j] + 1     # continue filling the table from the earlier table entry
+                    j += 1              # move j
         return t
 
     @staticmethod
@@ -35,20 +38,28 @@ class KMP:
         'Given strings s and p, find the starting index of the first occurrence p in s. Return -1 if there is none.'
         t = KMP.preprocess(p)
 
-        i = j = 0
+        i = j = 0   # start at the starts of s and p
         while i < len(s) and j < len(p):
+        # while both are not processed
             if s[i] == p[j]:
-                i += 1
-                j += 1
+            # if there's a character match
+                i += 1  # move i
+                j += 1  # move j
                 if j == len(p):
-                    return i - len(p)
+                # this means we've found the pattern
+                    return i - len(p)   # return the index of the start of the pattern
             else:
+            # if there isn't a character match
                 if j == 0:
-                    i += 1
+                # if we are at the start of the pattern
+                    i += 1  # just move on to the next char in the s
                 else:
-                    j = t[j-1]                
+                # if we are in the pattern,
+                    # we have already matched t[j-1] characters in s with p[:t[j-1]]
+                    # so just backtrack to the next character in p, the p[t[j-1]]
+                    j = t[j-1]
 
-        return -1
+        return -1   # we haven't found the pattern
 
 def print_preprocessing(p : str, t: list) -> None:
     'for preprocessing testing'
