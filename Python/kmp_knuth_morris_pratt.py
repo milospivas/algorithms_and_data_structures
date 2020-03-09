41964d5ae2cda4d8c16405edd3f5fabdc02a52fd
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-09 12:29:51 +0100

    Started KMP algorithm

---
+++ Python/kmp_knuth_morris_pratt.py
@@ -0,0 +1,32 @@
+""" author: Miloš Pivaš
+    date: 09.03.2020
+"""
+
+class KMP:
+    'Implements Knuth-Morris-Pratt algorithm for string pattern matching'
+    version = '0.1'
+
+    @staticmethod
+    def preprocess(p : str) -> list:
+        'Preprocess the string pattern p'
+
+        if len(p) == 0:
+            return []
+
+        t = [0 for _ in p]
+
+        j = 0
+        for i in range(1, len(p)):
+            if p[i] == p[j]:
+                t[i] = t[i-1] + 1
+                j += 1
+            else:
+                j = 0
+        return t
+
+
+# preprocess testing
+p = "abcdabca"
+t = KMP.preprocess(p)
+for c, i in zip(p, t):
+    print(c, i)
