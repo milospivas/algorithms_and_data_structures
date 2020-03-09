3a6e524ab8203c805dd78a727e6abbd2a95f413f
Miloš Pivaš <milos.pivas@gmail.com>
2020-03-09 13:12:27 +0100

    Fixed a bug in preprocessing

--- Python/kmp_knuth_morris_pratt.py
+++ Python/kmp_knuth_morris_pratt.py
@@ -22,13 +22,23 @@ class KMP:
                 j += 1
             else:
                 j = 0
+                if p[i] == p[j]:
+                    t[i] = 1
+                    j += 1
+                
         return t
 
 def print_preprocessing(p : str, t: list) -> None:
     'for preprocessing testing'
+    print('pattern char, preprocessing table entry')
     for c, i in zip(p, t):
         print(c, i)
 
+# preprocessing testing
 p = "abcdabca"
+t = KMP.preprocess(p)
+print_preprocessing(p, t)
+
+p = "abcdabcabcd"
 t = KMP.preprocess(p)
 print_preprocessing(p, t)
