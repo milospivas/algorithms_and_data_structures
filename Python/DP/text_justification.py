b11e6424a1e7a63250d6f6063f5164e5d4408b94
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-14 01:06:41 +0200

    added test() - tests text justfication algrorithm

--- Python/DP/text_justification.py
+++ Python/DP/text_justification.py
@@ -61,7 +61,8 @@ def justify_nr(words, line_width, i = 0):
     list
         A list of indices where to insert new lines.
     int
-        Accumulated badness score (='inf' if there is a word larger than the line_width).
+        Accumulated badness score 
+        (='inf' if there is a word larger than the line_width).
     '''
 
     # calculate the number of remaining words
@@ -90,3 +91,64 @@ def justify_nr(words, line_width, i = 0):
     return min_indices, min_score
 
 
+### testing
+
+def test(words, line_width, justfy_function):
+    ''' Tests given justification function.
+
+    Parameters
+    ----------
+    words : list
+        The list of words representing text to be justified.
+    line_width : int
+        Line width in number of characters.
+    justify_function : function handle
+        A function to use for justification.
+        The function needs to follow this format:
+    
+            Parameters
+            ----------
+            words : list
+                The list of words representing text to be justified.
+            line_width : int
+                Line width in number of characters.
+            [i : int]
+                Index of the word from which to start justifying.
+
+            Returns
+            -------
+            (list, int)
+            list
+                A list of indices where to insert new lines.
+            int
+                Accumulated badness score
+                (='inf' if there is a word larger than the line_width).
+    '''
+    
+    help(justfy_function)
+
+    indices, score = justfy_function(words, line_width)
+    indices.sort()
+    print('words:', words)
+    print('line_width:', line_width)
+    print('score:', score)
+    print('indices:', indices)
+
+    print('justified text:')
+    k = 0
+    for i, word in enumerate(words):
+        print(word, end='')
+        if i == indices[k]-1:
+            print('\n', end='')
+            k += 1
+        else:
+            print(' ', end='')
+
+
+text = '12345678901 123 123 123 123'
+words = text.split(' ')
+line_width = 11
+test(words, line_width, justify_nr)
+
+
+print('Exiting...')
