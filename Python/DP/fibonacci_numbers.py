e164f5547094ddbe2baf01b9bc5fd1dbaa76774b
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-13 22:04:02 +0200

    added recursion with memoization variant

--- Python/DP/fibonacci_numbers.py
+++ Python/DP/fibonacci_numbers.py
@@ -34,6 +34,48 @@ def fib_nr(n):
         return fib_nr(n-1) + fib_nr(n-2)
 
 
+### Recursion + caching (memoization)
+
+def fib_rc(n, cache = None):
+    ''' Returns n-th Fibonacci number.
+
+    Recursive function with caching (memoization)
+
+    Parameters
+    ----------
+    n : int
+        Index of the Fibonacci number to be returned.
+    
+    cache : dict
+        Cache (memo table) for storing previously calculated numbers.
+    
+    Returns
+    -------
+    int
+        n-th Fibonacci number (starting from 1, 1,...)
+    '''
+    
+    if n < 0:
+        return None
+
+    if n == 0:
+        return 0
+
+    elif n == 1:
+        return 1
+
+    else:
+        if cache is None:
+            cache = {}
+            
+        if n in cache:
+            return cache[n]
+        else:
+            f = fib_rc(n-1, cache) + fib_rc(n-2, cache)
+            cache[n] = f
+            return f
+
+
 ### testing
 
 def test_fib(fib, ins = None, outs = None):
@@ -62,7 +104,10 @@ def test_fib(fib, ins = None, outs = None):
     return True
 
 
-
+# testing naive recursion
 assert test_fib(fib_nr)
+
+# testing recursion with caching (memoization)
+assert test_fib(fib_rc)
 
 print('Exiting...')
