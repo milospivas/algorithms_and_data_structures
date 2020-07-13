84490092a90fa184e57b37844cf2ff839885d218
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-14 00:48:25 +0200

    added justify_nr() - a naive recursive DP solution

--- Python/DP/text_justification.py
+++ Python/DP/text_justification.py
@@ -39,3 +39,54 @@ def badness(words, i, j, line_width):
         b = float('inf')
     
     return b
+
+
+### Naive recursive DP
+
+def justify_nr(words, line_width, i = 0):
+    ''' Insert new lines into given text to justify it.
+
+    Parameters
+    ----------
+    words : list
+        The list of words representing text to be justified.
+    line_width : int
+        Line width in number of characters.
+    [i : int]
+        Index of the word from which to start justifying.
+
+    Returns
+    -------
+    (list, int)
+    list
+        A list of indices where to insert new lines.
+    int
+        Accumulated badness score (='inf' if there is a word larger than the line_width).
+    '''
+
+    # calculate the number of remaining words
+    n = len(words)
+    n_remaining = n-i
+
+    if n_remaining == 0:
+        return [i], 0
+
+    min_score = float('inf')
+
+    # for every of the remaining words
+    for j in range(i+1, n+1):
+    # pick a break-point for the new line
+        # justify the rest of words after new line
+        later_indices, score = justify_nr(words, line_width, j)
+        
+        # add the score for the current line
+        score += badness(words, i, j, line_width)
+        
+        # keep min
+        if score < min_score:
+            min_score = score
+            min_indices = later_indices + [j]
+
+    return min_indices, min_score
+
+
