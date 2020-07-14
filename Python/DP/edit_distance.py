f939419f8a5603f0d24a4ce086be381643f87d7e
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-15 01:19:38 +0200

    added naive recursive DP (without caching)

--- Python/DP/edit_distance.py
+++ Python/DP/edit_distance.py
@@ -25,8 +25,77 @@ class Operation:
         self.dy = dy
 
 
+### Naive recursive DP
+
+def edit_distance_nr(x, y, operations, i = 0, j = 0):
+    ''' Calculate edit distance between strings x and y.
+
+    Parameters
+    ----------
+    x : str
+        First string.
+    y : str
+        Second string.
+    operations : list
+        List of available operations
+    [i : int]
+        Starting index of the first string.
+    [j : int]
+        Starting index of the second string.
+
+    Returns
+    -------
+    int
+        Computetd edit distance.
+    '''
+
+    ''' Loking at x[i] and y[j], for each i and j, options are:
+            delete x[i],    costs 1
+            insert y[j],    costs 1
+            replace x[i] by y[j], costs 1
+    '''
+
+    if (i == len(x)) and (j == len(y)):
+        return 0
+    
+    min_cost = float('Inf')
+    # try available operations that surely transform the strings
+    for o in operations:
+        if (i + o.dx <= len(x)) and (j + o.dy <= len(y)):
+            cost = o.cost + edit_distance_nr(x, y, operations, i + o.dx, j + o.dy)
+
+            if cost < min_cost:
+                min_cost = cost
+    
+    # try not doing anything if characters already match:
+    if (i < len(x)) and (j < len(y)) and (x[i] == y[j]):
+        cost = 0 + edit_distance_nr(x, y, operations, i+1, j+1)
+        
+        if cost < min_cost:
+            min_cost = cost
+
+    return min_cost
+
+
+### testing
 # building operations list
 delete = Operation(1, 1, 0)
 insert = Operation(1, 0, 1)
 replace = Operation(1, 1, 1)
 operations = [delete, insert, replace]
+
+x = 'a'
+y = 'a'
+ed = edit_distance_nr(x, y, operations)
+print('x:', x)
+print('y:', y)
+print('ed:', ed)
+
+x = 'abacab'
+y = 'ahab'
+ed = edit_distance_nr(x, y, operations)
+print('x:', x)
+print('y:', y)
+print('ed:', ed)
+
+print('Exiting...')
