a287ee830e3c0bbd663cd8dc57c030745048cc85
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-16 02:16:02 +0200

    added recursive variant with caching (memoization)

--- Python/DP/knapsack.py
+++ Python/DP/knapsack.py
@@ -52,6 +52,69 @@ def knapsack_nr(m, v, M, n = None, i = 0):
     return val_excl, indices_excl
 
 
+# Recursive variant with caching (memoization)
+
+def knapsack_rc(m, v, M, n = None, i = 0, cache = None):
+    ''' Maximise the value of a knapsack, given the items and mass limit.
+
+    Recursive DP method, with caching.
+
+    Parameters
+    ----------
+    m : list
+        List of (int) masses of items.
+    v : list
+        List of (float/int) values of items.
+    M : int
+        Total mass limit of the knapsack.
+    [n : int]
+        Current mass limit.
+    [i : int]
+        Current item index.
+    [cache : dict]
+        Hashmap of already computed solutions.
+    
+    Returns
+    -------
+    (float, list)
+        float - total accumulated value.
+        list - items' indices
+    '''
+
+    if n is None:
+        n = M
+
+    if i == len(m):
+        return 0, []
+
+    if cache is None:
+        cache = {}
+
+
+    if (n, i+1) in cache:
+        val_excl, indices_excl = cache[(n, i+1)]
+    else:
+        val_excl, indices_excl = knapsack_rc(m, v, M, n, i + 1, cache)
+
+    sol = None
+
+    if n - m[i] >= 0:
+        if (n-m[i], i+1) in cache:
+            val_incl, indices_incl = cache[(n-m[i], i+1)]
+        else:
+            val_incl, indices_incl = knapsack_rc(m, v, M, n - m[i], i + 1, cache)
+            val_incl += v[i]
+
+        if val_incl > val_excl:
+            sol = val_incl, indices_incl + [i]
+    
+    if sol is None:
+        sol = val_excl, indices_excl
+    
+    cache[(n, i)] = sol
+    return sol
+    
+
 ### testing
 
 m = [1,2,3,4]
@@ -62,5 +125,12 @@ val, indices = knapsack_nr(m, v, M)
 
 print('val:', val)
 print('indices:', indices)
+
+
+val, indices = knapsack_nr(m, v, M)
+
+print('val:', val)
+print('indices:', indices)
+
 
 print('Exiting...')
