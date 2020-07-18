8ac4290d78ba463b59f0ed250c9982eae110d8e0
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-18 22:35:49 +0200

    clean-up in longest_common_subsequence()

--- Python/DP/longest_common_subsequence.py
+++ Python/DP/longest_common_subsequence.py
@@ -2,6 +2,10 @@
 
     Depends on: edit_distance.py
 
+    Works with iterables of any type, but, of course,
+    in realistic use cases, this would be implemented as a class method,
+    dedicated to specific type.
+
     Author: Miloš Pivaš
 '''
 
@@ -9,7 +13,7 @@ from edit_distance import Operation
 from edit_distance import edit_distance_bu
 
 
-def longest_common_subsequence(x, y, empty_object = None, append_func = None, is_inplace = None):
+def longest_common_subsequence(x, y, empty_object = None, append_func = None):
     ''' Finds the longest common subsequence in two given iterables.
 
     Subsequence is sequential but doesn't have to be contiguous.
@@ -26,8 +30,6 @@ def longest_common_subsequence(x, y, empty_object = None, append_func = None, is
         Empty object of the same class as x and y.
     append_func : function handle, optional
         Function that appends values to the end of the object of the same class as x and y.
-    is_inplace : bool, optional
-        Type of the append function. True if it operates in-place, False otherwise.
 
     Returns
     -------
@@ -37,39 +39,35 @@ def longest_common_subsequence(x, y, empty_object = None, append_func = None, is
     Raises
     ------
     Exception
-        'Please provide an empty object, the append operator and its type.'
+        'Please provide an empty object and the append operator.'
         If input isn't string or list, and the additional information isn't supplied.
     '''
 
-    insert = Operation(1, 0, 1)
-    delete = Operation(1, 1, 0)
+    insert = Operation(cost=1, dx=0, dy=1)
+    delete = Operation(cost=1, dx=1, dy=0)
     operations = [insert, delete]
 
     _, performed_operations = edit_distance_bu(x, y, operations)
 
     performed_operations.sort(key = lambda oij : oij[1])
 
-
     if type(x) is str:
-        empty_object, append_func, is_inplace = '', str.__add__, False
+        empty_object, append_func = '', str.__add__
 
     elif type(x) is list:
-        empty_object, append_func, is_inplace = [], list.append, True
+        empty_object, append_func = [], list.append
 
-    if (empty_object is None) or (append_func is None) or (is_inplace is None):
-        raise Exception('Please provide an empty object, the append operator and its type.')
+    if (empty_object is None) or (append_func is None):
+        raise Exception('Please provide an empty object and the append operator.')
 
-    s = empty_object
-    if is_inplace:
-        for o, i, j in performed_operations:
-            if o is None:
-                append_func(s, x[i])
-    else:
-        for o, i, j in performed_operations:
-            if o is None:
-                s = append_func(s, x[i])
+    subsequence = empty_object
+    for operation, i, j in performed_operations:
+        if (operation is not insert) and (operation is not delete):
+            append_return_value = append_func(subsequence, x[i])
+            if append_return_value is not None:
+                subsequence = append_return_value
 
-    return s
+    return subsequence
 
 
 # ### testing
@@ -96,7 +94,7 @@ def longest_common_subsequence(x, y, empty_object = None, append_func = None, is
 # x = np.array([1,4,2,5,3])
 # y = np.array([1,0,2,3,4])
 
-# lcs = longest_common_subsequence(x, y, np.array([]), np.append, False)
+# lcs = longest_common_subsequence(x, y, np.array([]), np.append)
 
 # assert np.all(lcs == np.array([1,2,3]))
 
