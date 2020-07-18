a8fef2b4c386bd136da09ef01f39273d13666cea
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-18 19:44:54 +0200

    clean-up in justify_rc()

--- Python/DP/text_justification.py
+++ Python/DP/text_justification.py
@@ -91,7 +91,7 @@ def justify_nr(words, line_character_limit):
 
 ### Recursive DP with caching (memoization)
 
-def justify_rc(words, line_width, i = 0, cache = None):
+def justify_rc(words, line_character_limit):
     ''' Insert new lines into given text to justify it.
 
     Recursive method using dynamic programming with caching (memoization).
@@ -100,56 +100,47 @@ def justify_rc(words, line_width, i = 0, cache = None):
     ----------
     words : list
         The list of words representing text to be justified.
-    line_width : int
+    line_character_limit : int
         Line width in number of characters.
-    i : int, optional
-        Index of the word from which to start justifying.
-    cache : dict, optional
-        Dictionary of already calculated solutions.
 
     Returns
     -------
     out : list, int
         list
-            A list of indices where to insert new lines.
+            A list of indices where new lines are to be inserted.
         int
             Accumulated badness score
-            (='inf' if there is a word larger than the line_width).
+            (='inf' if there is a word larger than line_character_limit).
     '''
 
-    # calculate the number of remaining words
-    n = len(words)
-    n_remaining = n-i
+    def cache_init():
+        cache = {}
+        cache[len(words)] = [], 0
+        return cache
 
-    if n_remaining == 0:
+    def __justify_with_caching(start = 0):
+
+        min_score = float('inf')
+        for next_start in range(start+1, len(words)+1):
+
+            next_indices, next_score = cache[next_start] if next_start in cache else __justify_with_caching(next_start)
+
+            score = next_score + badness(words, start, next_start, line_character_limit)
+
+            if score < min_score:
+                min_score = score
+                min_indices = next_indices + [next_start]
+
+        cache[start] = min_indices, min_score
+
+        return min_indices, min_score
+
+    if len(words) == 0:
         return [], 0
 
-    if cache is None:
-        cache = {}
-
-    min_score = float('inf')
-
-    # for every of the remaining words
-    for j in range(i+1, n+1):
-    # pick a break-point for the new line
-        # justify the rest of words after new line
-
-        if j in cache:
-            # get it from cache if it's already computed
-            later_indices, score = cache[j]
-        else:
-            # compute it
-            later_indices, score = justify_rc(words, line_width, j, cache)
-            cache[j] = (later_indices, score)
-
-        # add the score for the current line
-        score += badness(words, i, j, line_width)
-
-        # keep min
-        if score < min_score:
-            min_score = score
-            min_indices = later_indices + [j]
-
+    cache = cache_init()
+    min_indices, min_score = __justify_with_caching()
+    min_indices.sort()
     return min_indices, min_score
 
 
